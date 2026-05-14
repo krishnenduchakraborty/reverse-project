f03a9d8ff07f7e98024a1bdf63554018a74b2d88
krishnendu-Vision <ckrishnendu76@gmail.com>
2026-05-14 17:50:30 +0530

    Subtraction section is updated, in Java terminal calculator, subtract limit is set to the, -10 and above only.

--- My_Java_Calculator/Subtraction.java
+++ My_Java_Calculator/Subtraction.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
-
 public class Subtraction {
     public void show(double s, double p) {
-        double result = s - p;
-        System.out.println("Subtraction result: " + result);
+        if(s >= -10 && p >= -10) {           
+            double result = s - p;
+            System.out.println("Subtraction result: " + result);
+        }
+        else {          
+            System.err.println("Subtraction limit is, set to -10 and above only!");
+        }
     }
 }
