f07b037fa5dabd7cce497df55bc84ccb17ad0ec5
krishnendu-Vision <ckrishnendu76@gmail.com>
2026-05-14 10:21:37 +0530

    Java Calculator, Addition logic update

--- My_Java_Calculator/Addition.java
+++ My_Java_Calculator/Addition.java
@@ -1,6 +1,11 @@
 public class Addition {
     public void show(double a, double b) {
-        double result = a + b;
-        System.out.println("Addition result is: " +result);
+        if(a > 0 && b > 0) {
+        double result = a + b; 
+        System.out.println("Addition result is: " + result);  
+        }
+        else {
+            System.err.println("Entered value should be, greater then zero!");
+        }
     }
 }
