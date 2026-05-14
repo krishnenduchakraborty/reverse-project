ad11d0c1a2b33226e0eb7e151526bd5dd6c5d3fb
Vision <ckrishnendu76@gmail.com>
2026-05-14 23:17:53 +0530

    Fix error message for division by zero

--- My_Java_Calculator/Division.java
+++ My_Java_Calculator/Division.java
@@ -2,8 +2,8 @@ public class Division {
     public void show(double input1, double input2) {
         if(input1 == 0 && input2 == 0) {
             System.err.println("Cannot divide with zero!.");
-        } else if(input1 > 0 && input2 == 0) {
-            System.err.println("input two should be contain some value!.");
+        } else if(input2 == 0) {
+            System.err.println("input two should contain some value!.");
         } else {
             double result = input1 / input2;
             System.out.println("\nDivision result: " + result + "\n");
