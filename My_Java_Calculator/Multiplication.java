9016cc19e6ca0a312ae1d04210ce7334146f556a
krishnendu-Vision <ckrishnendu76@gmail.com>
2026-05-17 12:35:02 +0530

    Updating files!

--- My_Java_Calculator/Multiplication.java
+++ My_Java_Calculator/Multiplication.java
@@ -1,6 +1,10 @@
 public class Multiplication {
-    public void show(double m, double t) {
-        double result = m * t;
+    public void show(double mul1, double mul2) {
+        if(mul1 <= 0 || mul2 <= 0) {
+            System.err.println("Input one and two,\nshould contain value!.");
+        }else {
+        double result = mul1 * mul2;
         System.out.println("Multiplication result: " + result);
+        }
     }
 }
